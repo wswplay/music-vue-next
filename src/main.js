559c45f7e17350ed873d1405c67720a41727e49f
xiao <183627391@qq.com>
2021-12-31 20:03:45 +0800

    image v-lazy loading

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import lazyPlugin from "vue3-lazy";
 
 // 引入全局样式
 import "@/assets/style/index.less";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(lazyPlugin, {
+    loading: require("@/assets/images/default.png"),
+  })
+  .mount("#app");
