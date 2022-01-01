b626d50501d9519ad5220a180ad52f82d338a15e
xiao <183627391@qq.com>
2022-01-01 19:56:18 +0800

    fixed title transform

--- src/components/index-list/use-fixed.js
+++ src/components/index-list/use-fixed.js
@@ -1,15 +1,28 @@
 import { computed, nextTick, ref, watch } from "vue";
 
 export default function useFixed(props) {
+  const TITLE_HEIGHT = 30;
   const groupRef = ref(null);
   const listHeights = ref([]);
   const scrollY = ref(0);
   const curIndex = ref(0);
+  const distance = ref(0);
 
   const fixedTitle = computed(() => {
     if (scrollY.value < 0) return "";
     const curGroup = props.data[curIndex.value];
     return (curGroup && curGroup.title) || "";
+  });
+
+  const fixedStyle = computed(() => {
+    const distanceVal = distance.value;
+    const diff =
+      distanceVal > 0 && distanceVal < TITLE_HEIGHT
+        ? distanceVal - TITLE_HEIGHT
+        : 0;
+    return {
+      transform: `translate3d(0, ${diff}px, 0)`,
+    };
   });
 
   watch(
@@ -26,6 +39,7 @@ export default function useFixed(props) {
       const heightBottom = listHeiVal[i + 1];
       if (newY >= heightTop && newY <= heightBottom) {
         curIndex.value = i;
+        distance.value = heightBottom - newY;
       }
     }
   });
@@ -52,5 +66,6 @@ export default function useFixed(props) {
     groupRef,
     onScroll,
     fixedTitle,
+    fixedStyle,
   };
 }
